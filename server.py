1c4381533d91458b68baa5bde9d58e3680643217
Boredalien248 <kaustubh248@gmail.com>
2023-09-28 05:55:16 -0600

    part sol valid-res(fixing)

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 #  coding: utf-8 
 import socketserver
+import os.path
+import datetime
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -30,15 +32,88 @@ import socketserver
 class MyWebServer(socketserver.BaseRequestHandler):
     
     def handle(self):
+        format = 'utf-8'
+        # receives the request 
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        # printing the request received 
+        print(f"Got a request of: {self.data}\n")
+        
+        received_request = self.data.decode(format)
+        request_info = received_request.splitlines()[0]
+        # print statements commented out below used for self testing 
+        # print(request_info)
+        # print(method)
+        # print(path)
+        # print(not_needed)
+        # checks if there are errors 
+        final_response = self.test_if_valid(request_info)
+
+        # if there are no errors execute then execute line in if condition
+        if final_response == b'':
+            print('worked')
+            final_response = self.valid_response(request_info)
+        
+        # send the response to the client
+        self.request.sendall(final_response)
+
+    def test_if_valid(self, request_info):
+        method, path, http_version = request_info.split(" ")
+        if request_info == "":
+            return b'HTTP/1.1 400 Bad Request\r\n'
+        if method != "GET":
+            return b'HTTP/1.1 405 Method Not Allowed\r\n'
+        if http_version != "HTTP/1.1":
+            return b'HTTP/1.1 400 Bad Request\r\n'
+        if os.path.exists('www' + path) == False:
+            return b'HTTP/1.1 404 Not Found \r\n'
+        if os.path.exists('www' + path):
+            return b''
+    
+    def valid_response(self, request_info):
+        method, path, http_version = request_info.split(" ")
+        main_path = 'www' + path
+        if os.path.exists('www' + path):
+            if os.path.isfile('www' + path) == False:
+                if path[-1] == '/':
+                    main_path += 'index.html'
+
+                    if os.path.exists(main_path) == False:
+                        return b'HTTP/1.1 404 Not Found \r\n'
+            
+                if path[-1] != '/':
+                    current_datetime = datetime.datetime.utcnow()
+                    formatted_date = current_datetime.strftime('%a, %d %b %Y %H:%M:%S GMT')
+                    return b'HTTP/1.1 301 Moved Permanently \r\n' + b'Location: ' + path.encode('utf-8') + b'/\r\n' + b'Date: ' + formatted_date.encode('utf-8') + b' \r\n'
+
+            with open(main_path, 'rb') as fh:
+                file_data = fh.read().encode('utf-8')
+
+            length_file_data = (str(len(file_data))).encode('utf-8')
+
+            current_datetime = datetime.datetime.utcnow()
+            formatted_date = current_datetime.strftime('%a, %d %b %Y %H:%M:%S GMT')
+
+            file_format = main_path.split('.')[-1]
+            main_file_format = b''
+            if file_format == 'html':
+                main_file_format = b'Content type: text/html \r\n'
+            elif file_format == 'css':
+                main_file_format = b'Content type: text/css \r\n'
+            else: 
+                main_file_format = b'Content type: ' + file_format.encode('utf-8') + b' \r\n'
+
+            return b'HTTP/1.1 200 OK \r\n' + b'Date: ' + formatted_date.encode('utf-8') + b' \r\n' + main_file_format + b'Content-length: ' + length_file_data + b' \r\n' + file_data
+
+        else:
+            return b'HTTP/1.1 404 Not Found \r\n'
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
 
     socketserver.TCPServer.allow_reuse_address = True
     # Create the server, binding to localhost on port 8080
+    # displays that the server is on 
+    print("SERVER HAS STARTED ........... \n")
     server = socketserver.TCPServer((HOST, PORT), MyWebServer)
 
     # Activate the server; this will keep running until you
